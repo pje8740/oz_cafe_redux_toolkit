0c3b56eff6d63b5df8cded797e66270c95a88b4b
pje8740 <pje8740@cu.ac.kr>
2025-10-16 09:37:39 +0900

    ordermodal수정

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -3,58 +3,61 @@ import { useDispatch } from "react-redux";
 import data from "../assets/data";
 import { addToCart } from "../redux/redux";
 
-function OrderModal({ modalMenu, setModalOn, cart, setCart }) {
+function OrderModal({ modalMenu, setModalOn }) {
   const dispatch = useDispatch();
   const [options, setOptions] = useState({ 온도: 0, 진하기: 0, 사이즈: 0 });
   const [quantity, setQuantity] = useState(1);
   const itemOptions = data.options;
-  console.log(options);
+
+  const handleAddToCart = () => {
+    const itemToAdd = {
+      ...modalMenu,
+      options,
+      quantity,
+    };
+    dispatch(addToCart(itemToAdd));
+    setModalOn(false);
+  };
+
+  if (!modalMenu) return null;
+
   return (
     <>
-      {modalMenu ? (
-        <section className="modal-backdrop" onClick={() => setModalOn(false)}>
-          <div className="modal" onClick={(e) => e.stopPropagation()}>
-            <div className="modal-item">
-              <img src={modalMenu.img} />
-              <div>
-                <h3>{modalMenu.name}</h3>
-                <div>{modalMenu.description}</div>
-              </div>
-            </div>
-            <ul className="options">
-              {Object.keys(itemOptions).map((el) => (
-                <Option
-                  key={el}
-                  options={options}
-                  setOptions={setOptions}
-                  name={el}
-                  itemOptions={itemOptions[el]}
-                />
-              ))}
-            </ul>
-            <div className="submit">
-              <div>
-                <label htmlFor="count">개수</label>
-                <input
-                  id="count"
-                  type="number"
-                  value={quantity}
-                  min="1"
-                  onChange={(event) => setQuantity(Number(event.target.value))}
-                />
-              </div>
-              <button
-                onClick={() => {
-                  setCart([...cart, { options, quantity, id: modalMenu.id }]);
-                  setModalOn(false);
-                }}
-              >
-                장바구니 넣기
-              </button>
+      <section className="modal-backdrop" onClick={() => setModalOn(false)}>
+        <div className="modal" onClick={(e) => e.stopPropagation()}>
+          <div className="modal-item">
+            <img src={modalMenu.img} />
+            <div>
+              <h3>{modalMenu.name}</h3>
+              <div>{modalMenu.description}</div>
             </div>
           </div>
-        </section>
-      ) : null}
+          <ul className="options">
+            {Object.keys(itemOptions).map((el) => (
+              <Option
+                key={el}
+                options={options}
+                setOptions={setOptions}
+                name={el}
+                itemOptions={itemOptions[el]}
+              />
+            ))}
+          </ul>
+          <div className="submit">
+            <div>
+              <label htmlFor="count">개수</label>
+              <input
+                id="count"
+                type="number"
+                value={quantity}
+                min="1"
+                onChange={(event) => setQuantity(Number(event.target.value))}
+              />
+            </div>
+            <button onClick={handleAddToCart}>장바구니 넣기</button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
