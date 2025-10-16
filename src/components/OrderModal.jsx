72cfd8bef81cb3fb0b78caeca221ccba9e6967ca
pje8740 <pje8740@cu.ac.kr>
2025-10-16 09:30:34 +0900

    App수정, 메뉴 수정

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import data from "../assets/data";
+import { addToCart } from "../redux/redux";
 
 function OrderModal({ modalMenu, setModalOn, cart, setCart }) {
+  const dispatch = useDispatch();
   const [options, setOptions] = useState({ 온도: 0, 진하기: 0, 사이즈: 0 });
   const [quantity, setQuantity] = useState(1);
   const itemOptions = data.options;
